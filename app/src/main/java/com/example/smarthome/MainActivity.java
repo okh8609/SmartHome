d768f188393f9da3efe356a6caa0ad0f6e60a246
Khaos <okh8609@gmail.com>
2020-11-25 12:07:58 +0800

    可以接收到 SMS msg 了
    缺：接收到訊息後，要更新UI。

--- app/src/main/java/com/example/smarthome/MainActivity.java
+++ app/src/main/java/com/example/smarthome/MainActivity.java
@@ -1,18 +1,30 @@
 package com.example.smarthome;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_PERMISSIONS_REQUEST_RECEIVE_SMS = 80;
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 88;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        getSupportActionBar().hide(); //hide the title bar
+
+        checkSmsPermission();
 
         //允許執行緒從網路下載
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
@@ -22,5 +34,24 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.fragment, MainFragment.newInstance());
         fragmentTransaction.commit();
+    }
+
+    private void checkSmsPermission() {
+        // 尚未授予權限。 使用requestPermissions（）。
+        // MY_PERMISSIONS_REQUEST_RECEIVE_SMS是一個應用程序定義的int常量。
+        // 回調方法獲取請求的結果。
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.RECEIVE_SMS},
+                    MY_PERMISSIONS_REQUEST_RECEIVE_SMS);
+        }
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.SEND_SMS},
+                    MY_PERMISSIONS_REQUEST_SEND_SMS);
+        }
     }
 }
