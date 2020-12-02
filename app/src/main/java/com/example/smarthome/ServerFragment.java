5abcf05c95dd66ca7680392643aa75edc30c4ae2
Khaos <okh8609@gmail.com>
2020-12-02 10:19:51 +0800

    接收到的sms訊息，可以顯示在UI上

--- app/src/main/java/com/example/smarthome/ServerFragment.java
+++ app/src/main/java/com/example/smarthome/ServerFragment.java
@@ -1,9 +1,17 @@
 package com.example.smarthome;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -19,12 +27,55 @@ public class ServerFragment extends Fragment {
         return fragment;
     }
 
+    BroadcastReceiver broadcastReceiver;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.server_fragment, container, false);
+        final View root = inflater.inflate(R.layout.server_fragment, container, false);
 
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("android.provider.Telephony.SMS_RECEIVED");
+        //filter.setPriority(18);
+        getContext().registerReceiver(broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Bundle bundle = intent.getExtras();
+
+                String sPhoneNo = "";
+                String sMsgBody = "";
+
+                if (bundle != null) {
+                    //The messages are stored in an Object array in the PDU format.
+                    Object[] pdus = (Object[]) bundle.get("pdus");
+                    SmsMessage[] smsMsgs = new SmsMessage[pdus.length];
+                    for (int i = 0; i < pdus.length; i++)
+                        smsMsgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+
+                    //取得簡訊發送人的電話號碼
+                    sPhoneNo = smsMsgs[0].getDisplayOriginatingAddress();
+                    
+                    //取得簡訊內容
+                    for (SmsMessage msg : smsMsgs)
+                        sMsgBody += msg.getDisplayMessageBody();
+                }
+
+                Log.i("SmsReceiver", "sPhoneNo:" + sPhoneNo);
+                Log.i("SmsReceiver", "sMsgBody:" + sMsgBody);
+
+                String msg = "## 號碼：" + sPhoneNo + "\n## 內容：" + sMsgBody;
+                // Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+                TextView server_log = (TextView) root.findViewById(R.id.server_log);
+                server_log.setText(msg + "\n\n\n" + server_log.getText());
+            }
+        }, filter);
 
         return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        getContext().unregisterReceiver(broadcastReceiver);
     }
 }
